ec4ab4b219ddd3df7e9aee69c23c61cf396fb680
taznimt <taznimanwar06@gmail.com>
2025-10-21 10:17:25 +0700

    Menambahkan app.listen agar server dapat dijalankan

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ app.use(express.urlencoded({extended: true}));
 app.get('/',(req,res)=> {
     res.send('Hello World!');
 });
+
+app.listen(PORT, ()=> {
+    console.log('Server is running on port ${PORT}');
+});
